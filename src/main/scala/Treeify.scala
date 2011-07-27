369e1e7d0ad0af930a6096befeb0d1aca33b4e10
Leif Warner <abimelech@gmail.com>
2011-07-26 20:33:04 -0700

    Skip JSON lib & generate (minimized) JSON directly.

--- src/main/scala/Treeify.scala
+++ src/main/scala/Treeify.scala
@@ -4,32 +4,27 @@ object Treeify {
 
 def localName(res:UriRef) = ".*[#/]".r.split(res.uri)(1)
 
+def str(s:String) = "\"" + s + "\""
+def obj(m:Map[String,String]) = m.map(Function.tupled(str(_) +":"+ _)).mkString("{",",","}")
+
 def apply(start:GraphNode) = {
   val graph = start.graph
 
-  def treeify(subject:SubjectNode, visited:Set[SubjectNode]):Any = {
+  def treeify(subject:SubjectNode, visited:Set[SubjectNode]):String = {
     val data = graph.triplesLike(subject,UriRef,Node) collect {
-      case RdfTriple(_, RDF.Type, typ:UriRef) => "type" -> localName(typ)
-      case RdfTriple(_, pred, value:SubjectNode) if !visited.contains(value)=>localName(pred) -> treeify(value, visited + subject) 
+      case RdfTriple(_, RDF.Type, typ:UriRef) => ("type", str(localName(typ)))
+      case RdfTriple(_, pred, value:SubjectNode) if !visited.contains(value) => (localName(pred), treeify(value, visited + subject))
       case RdfTriple(_, pred, TypedLiteral(value, datatype)) => (localName(pred), datatype match {
-        case XSD.int | XSD.integer => value.toInt
-        case XSD.float | XSD.double => value.toFloat
-        case XSD.boolean => value.toBoolean
-        case other => value
+        case XSD.int | XSD.integer | XSD.float | XSD.double | XSD.decimal | XSD.boolean => value
+        case other => str(value) 
       })
-      case RdfTriple(_, pred, PlainLiteral(value, _)) => localName(pred) -> value
-    } groupBy (_._1) mapValues (set=>
-        if (set.size == 1)
-          set.head._2
-        else
-          set.map(_._2)
-      )
+      case RdfTriple(_, pred, PlainLiteral(value, _)) => (localName(pred), str(value))
+    } groupBy (_._1) mapValues (_.map(_._2).mkString("[",",","]"))
 
-    subject match {
-      case UriRef(uri) if data.isEmpty => uri
-      case UriRef(uri) => data + ("uri" -> uri)
+    obj(subject match {
+      case UriRef(uri) => data + ("uri" -> str(uri))
       case Blank(_) => data
-    }
+    })
   }
 
   treeify(start.node, Set(start.node))
--- src/test/scala/TreeifySpec.scala
+++ src/test/scala/TreeifySpec.scala
@@ -18,21 +18,9 @@ class TreeifySpec extends FlatSpec with ShouldMatchers {
       bill -name-> "Bill"
     )
 
-    Treeify(graph/leif) should equal (Map(
-      "uri"->leif.uri, 
-      "name"->"Leif", 
-      "homepage"->"http://leif.com/home",
-      "knows"->Set(
-        Map(
-          "uri"->bill.uri, 
-          "name"->"Bill"
-        ),
-        Map(
-          "uri"->john.uri, 
-          "name"->"John"
-        )
-      )
-    ))
+    Treeify(graph/leif) should equal (
+      """{"name":["Leif"],"knows":[{"name":["Bill"],"uri":"http://bill.com"},{"name":["John"],"uri":"http://john.com"}],"homepage":[{"uri":"http://leif.com/home"}],"uri":"http://leif.com"}"""
+    )
   }
 
 }
