9418d1f1689971ca5d429999e84d820468981744
Leif Warner <abimelech@gmail.com>
2011-07-26 18:34:39 -0700

    Simplied view, as prophesied in the README.

--- src/test/scala/TreeifySpec.scala
+++ src/test/scala/TreeifySpec.scala
@@ -6,24 +6,30 @@ import org.scalatest.matchers.ShouldMatchers
 
 class TreeifySpec extends FlatSpec with ShouldMatchers {
   "A graph" should "be represented as a tree" in {
-    val List(leif, john, bill, name, knows) = List("http://leif.com", "http://john.com", "http://bill.com", "http://schema.com/name", "http://schema.com/knows").map( UriRef(_) )
+    val List(leif, john, bill, name, knows, homepage) = 
+      List("http://leif.com", "http://john.com", "http://bill.com", "http://schema.com/name", "http://schema.com/knows", "http://schema.com/homepage").map( UriRef(_) )
     val graph = Graph(
-      leif -(name->"Leif", knows-> ObjSet(bill, john)),
-      john-name->"John",
-      bill-name->"Bill"
+      leif -(
+        name ->"Leif", 
+        homepage -> UriRef("http://leif.com/home"),
+        knows -> ObjSet(bill, john)
+      ),
+      john -name-> "John",
+      bill -name-> "Bill"
     )
 
     Treeify(graph/leif) should equal (Map(
       "uri"->leif.uri, 
-      "name"->Set("Leif"), 
+      "name"->"Leif", 
+      "homepage"->"http://leif.com/home",
       "knows"->Set(
         Map(
           "uri"->bill.uri, 
-          "name"->Set("Bill")
+          "name"->"Bill"
         ),
         Map(
           "uri"->john.uri, 
-          "name"->Set("John")
+          "name"->"John"
         )
       )
     ))
