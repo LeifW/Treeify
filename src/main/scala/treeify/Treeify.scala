3bd2aee693942f19457d12fb4777389d27521ad1
Leif Warner <abimelech@gmail.com>
2013-12-22 14:28:24 -0800

    Cleanup

--- src/main/scala/treeify/Treeify.scala
+++ src/main/scala/treeify/Treeify.scala
@@ -1,22 +1,14 @@
 package treeify
 
-import org.json4s.JsonAST._
-import org.scardf._
-//import scalaz.Writer
-import scalaz.syntax.writer._
-import scalaz._
-//import Scalaz._
 import effectful._
+import scalaz.{Writer, Semigroup}
+import scalaz.syntax.writer._
 import scalaz.syntax.applicative._
 import scalaz.syntax.traverse._
-//import scalaz.syntax.monad._
-import scalaz.std.map._
 import scalaz.std.list._
-import org.scardf.RdfTriple
-import org.json4s.JsonAST.JString
-import org.scardf.GraphNode
-
-//import Scalaz._
+import scalaz.std.map._
+import org.scardf._
+import org.json4s.JsonAST._
 
 object PropertyInfo extends Enumeration {
   val id = Value("@id")
@@ -36,24 +28,15 @@ object Treeify {
     val local = lastDelimiter.split(res.uri)(1)
     local set Map(local -> res)
   }
-  //def apply(start:GraphNode): Context[JObject] = null
 
   def apply(start:GraphNode) = {
     val graph = start.graph
 
     def treeify(subject:SubjectNode, visited:Set[SubjectNode]): Context[JObject] =
-      //val data: List[Context[(String, _ <: JValue)]] = graph.triplesLike(subject,UriRef,Node) collect {
       (graph.triplesLike(subject, UriRef, Node) collect {
         case RdfTriple(_, RDF.Type, typ: UriRef) => effectfully { ("@type" -> JString(localName(typ)!)) }
-        //case RdfTriple(_, pred, value:SubjectNode) if !visited.contains(value)=> localName(pred) flatMap (_ -> treeify(value, visited + subject))
         case RdfTriple(_, pred, value: SubjectNode) if !visited.contains(value) =>
            effectfully { localName(pred).! -> treeify(value, visited + subject).! }
-        /*
-          for {
-            localPred <- localName(pred)
-            subtree <- treeify(value, visited + subject)
-          } yield localPred -> subtree
-          */
         case RdfTriple(_, pred, TypedLiteral(value, datatype)) => for {
           l <- localName(pred)
           d <- datatype match {
@@ -63,15 +46,6 @@ object Treeify {
             case other => JString(value).point[Context]
           }
         } yield l -> d
-        /*
-        case RdfTriple(_, pred, TypedLiteral(value, datatype)) => localName(pred) flatMap (_ ->
-          datatype match {
-            case XSD.int | XSD.integer => JInt(value.toInt).point[Context]
-            case XSD.float | XSD.double => JDouble(value.toDouble).point[Context]
-            case XSD.boolean => JBool(value.toBoolean).point[Context]
-            case other => JString(value).point[Context]
-        })
-        */
         case RdfTriple(_, pred, PlainLiteral(value, _)) =>
           effectfully{ (localName(pred).! -> JString(value)) }
       }).toList.sequenceU map {data =>
@@ -83,16 +57,6 @@ object Treeify {
           }
         )
       }
-
-      /*
-      JObject( subject match {
-        //case Blank(_) => data
-        //case UriRef(uri) => ("@id" -> JString(uri)) :: data
-        //case UriRef(uri) => ("@id" -> JString(uri))
-        case UriRef(uri) =>
-          data.sequenceU //map (v => List("@id" -> JString(uri)) :: v)
-      }).point[Context]
-      */
 
     treeify(start.node, Set(start.node)).run
   }
