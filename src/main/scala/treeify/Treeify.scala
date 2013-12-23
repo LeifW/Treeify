b13b068417b0094abaa7cfa2e6292763b6323359
Leif Warner <abimelech@gmail.com>
2013-12-23 00:55:08 -0800

    Semigroup instance for These, FUCK YEAH

--- src/main/scala/treeify/Treeify.scala
+++ src/main/scala/treeify/Treeify.scala
@@ -1,15 +1,17 @@
 package treeify
 
 import effectful._
-import scalaz.{Writer, Semigroup}
+import scalaz.{\&/, Writer, Semigroup, Equal}
 import scalaz.syntax.writer._
 import scalaz.syntax.applicative._
 import scalaz.syntax.traverse._
 import scalaz.std.list._
 import scalaz.std.map._
+import scalaz.syntax.equal._
 import org.scardf._
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
+import scalaz.\&/.{Both, That, This}
 
 /*
 object PropertyInfo extends Enumeration {
@@ -25,16 +27,27 @@ case class Typ(t: UriRef) extends PropertyInfo("@type")
 
 object Treeify {
 
-  type Context[A] = Writer[Map[String, UriRef], A]
+  type Context[A] = Writer[Map[String, Id \&/ Typ], A]
   implicit val groupie = Semigroup.instance[UriRef]{ case (UriRef(u1), UriRef(u2)) =>
     if (u1 != u2) sys.error(u1 + " did not equal " + u2)
     UriRef(u1)
   }
 
+  implicit val idEq = Equal.equalA[Id]
+  implicit val typEq = Equal.equalA[Typ]
+
+  implicit val theseAdd = Semigroup.instance[Id \&/ Typ] {
+    case (This(t1), That(t2)) => Both(t1, t2)
+    case (t1, t2) if t1 === t2 => t1
+    case (Both(a, b), o) if o.bimap(_ === a, _ === b).foldRight(true)(_ && _) => Both(a, b)
+    case (o, Both(a, b)) if o.bimap(_ === a, _ === b).foldRight(true)(_ && _) => Both(a, b)
+    case (t1, t2) => sys.error("Conflicting information in document: " + t1 + " & " + t2)
+  }
+
   val lastDelimiter = ".*[#/]".r
   def localName(res:UriRef): Context[String] = {
     val local = lastDelimiter.split(res.uri)(1)
-    local set Map(local -> res)
+    local set Map(local -> This(Id(res)))
   }
 
   def apply(start:GraphNode) = {
@@ -68,7 +81,7 @@ object Treeify {
       }
 
     val (context, json ) = treeify(start.node, Set(start.node)).run
-    JField("@context", context.mapValues(_.uri)) ~ json
+    JField("@context", context.collect { case (k, This(Id(UriRef(uri)))) => JField(k,  uri)}) ~ json
   }
 
 }
